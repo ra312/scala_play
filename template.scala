77c5588c6bf143e780f9ee2ae1482d4ee9b817ae
ra312 <ra312@imperial.ac.uk>
2019-05-29 21:43:37 +0100

    Compiled tick data class

---
+++ template.scala
@@ -0,0 +1,9 @@
+// A comment!
+/* Another comment */
+/** A documentation comment*/
+object MyModule {
+    def MyMethod(x: Int) = {}
+    def main(args:Array[String]):Unit = {
+        println("The main entry point")
+    }
+}
--- tick_class.scala
+++ tick_class.scala
@@ -1,6 +1,15 @@
-class TickData
-{
-    def Init() = println("This is a tick data class !")
+import java.util.Calendar
+class TickData {
+   var Ticker = Set("NYSE", "NASDAQ", "TSX","SSE","TSE", "Euronext")
+   val timestamp = Calendar.getInstance().getTime()
+   // volume
+   // bid
+   // offer
+
+   def list_markets():Unit = 
+   {
+      Ticker.foreach(T => println(T))
+      //println(Ticker)
+   }
 }
 
-val TickData = new TickData()
